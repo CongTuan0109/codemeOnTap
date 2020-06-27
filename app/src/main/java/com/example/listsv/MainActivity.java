ad509e618325ffdb4607efbc08f134f63c987135
CongTuan0109 <daotuan465@gmail.com>
2020-06-26 20:37:32 -0700

    codemedssv

--- app/src/main/java/com/example/listsv/MainActivity.java
+++ app/src/main/java/com/example/listsv/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -26,52 +29,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btngetjson=findViewById(R.id.buttonGetJson);
-        edittext=findViewById(R.id.editText);
 
-        btngetjson.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    InputStream inputStream =getAssets().open("son.json");
-                    InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
-                    BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+        try {
+            InputStream inputStream =getAssets().open("son.json");
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
 
-                    StringBuilder stringBuilder=new StringBuilder();
-                    String line=bufferedReader.readLine();
+            StringBuilder stringBuilder=new StringBuilder();
+            String line=bufferedReader.readLine();
 
-                    while(line!=null)
-                    {
-                        stringBuilder.append(line);
-                        line=bufferedReader.readLine();
-                    }
-                    bufferedReader.close();
-                    inputStreamReader.close();
-                    inputStream.close();
-                    //edittext.setText(stringBuilder.toString());
-
-                    //Qua trinh dua file thanh danh sach
-                    JSONObject jsonObject=new JSONObject(stringBuilder.toString());
-                    JSONArray jsonArray=jsonObject.getJSONArray("Sinhviens");
-                    JSONObject obj;
-                    for(int i=0;i<jsonArray.length();i++)
-                    {
-                        obj=jsonArray.getJSONObject(i);
-                        sinhvien=new SinhVien(
-                                obj.getString("ten"),
-                                obj.getInt("namsinh"),
-                                obj.getString("lop")
-                        );
-                        arraylistSV.addSinhVien(sinhvien);
-                    }
-                    Toast.makeText(MainActivity.this,""+arraylistSV.dssv.size(),Toast.LENGTH_SHORT).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            while(line!=null)
+            {
+                stringBuilder.append(line);
+                line=bufferedReader.readLine();
             }
-        });
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
+            //edittext.setText(stringBuilder.toString());
+
+            //Qua trinh dua file thanh danh sach
+            JSONObject jsonObject=new JSONObject(stringBuilder.toString());
+            JSONArray jsonArray=jsonObject.getJSONArray("Sinhviens");
+            JSONObject obj;
+            for(int i=0;i<jsonArray.length();i++)
+            {
+                obj=jsonArray.getJSONObject(i);
+                sinhvien=new SinhVien(
+                        obj.getString("ten"),
+                        obj.getInt("namsinh"),
+                        obj.getString("lop")
+                );
+                arraylistSV.addSinhVien(sinhvien);
+            }
+
+            ListView lv=findViewById(R.id.listDSSV);
+            SVadapter sVadapter=new SVadapter(this,R.layout.layoutdong,arraylistSV.dssv);
+            lv.setAdapter(sVadapter);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- app/src/main/java/com/example/listsv/SVadapter.java
+++ app/src/main/java/com/example/listsv/SVadapter.java
@@ -1,4 +1,37 @@
 package com.example.listsv;
 
-public class SVadapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class SVadapter extends ArrayAdapter<SinhVien> {
+    private Context context;
+    private int layoutid;
+    private List<SinhVien> sinhViens;
+    public SVadapter(@NonNull Context context, int resource, @NonNull List<SinhVien> objects) {
+        super(context, resource, objects);
+        this.context=context;
+        layoutid=resource;
+        sinhViens=objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView==null)
+            convertView= LayoutInflater.from(parent.getContext()).inflate(R.layout.layoutdong,parent,false);
+
+        TextView txtten=convertView.findViewById(R.id.txtTen);
+        txtten.setText(sinhViens.get(position).getTen());
+
+        return convertView;
+    }
 }
